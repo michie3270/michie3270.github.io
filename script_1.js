73e5cfb2a6f10185282c8066f635d3ca5a473682
Michelle Lee <tl428@cornell.edu>
2016-12-25 16:27:43 -0500

    initial outline

--- script_1.js
+++ script_1.js
@@ -1,7 +1,28 @@
-var amount = 400;
+//Scroll to feature for navigation
+$("#abouttab").click(function() {
 
-if (amount > 1000){
-	alert("hello");
-}else{
-	alert("no hello");
-}
+    $('html, body').animate({
+        scrollTop: $(".about_me").offset().top
+    }, 2000);
+});
+
+$("#portfotab").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".portfolio").offset().top
+    }, 2000);
+});
+
+$("#reltab").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".relCourse").offset().top
+    }, 2000);
+});
+
+$("#contacttab").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".site-footer").offset().top
+    }, 2000);
+});
