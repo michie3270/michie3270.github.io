36f36dda522557d9e39090395ad5e415ab3808ad
Michelle Lee <tl428@cornell.edu>
2017-10-22 23:05:02 -0400

    minor changes

--- script_1.js
+++ script_1.js
@@ -3,21 +3,21 @@ $("#abouttab").click(function() {
 
     $('html, body').animate({
         scrollTop: $(".about_me").offset().top
-    }, 2000);
+    }, 800);
 });
 
 $("#portfotab").click(function() {
 
     $('html, body').animate({
         scrollTop: $(".portfolio").offset().top
-    }, 2000);
+    }, 800);
 });
 
 $("#reltab").click(function() {
 
     $('html, body').animate({
         scrollTop: $(".relCourse").offset().top
-    }, 2000);
+    }, 800);
 });
 
 
@@ -25,5 +25,5 @@ $("#home").click(function() {
 
     $('html, body').animate({
         scrollTop: $(".pagetop").offset().top
-    }, 2000);
+    }, 800);
 });
